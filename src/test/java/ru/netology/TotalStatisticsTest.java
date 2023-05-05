e260c1ecb8d21a9ebb3fdac74ddbde53918fb40a
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2023-05-05 18:14:38 +0300

    test optimized

--- src/test/java/ru/netology/TotalStatisticsTest.java
+++ src/test/java/ru/netology/TotalStatisticsTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 class TotalStatisticsTest {
-//    Statistics testStat;
-//    List<Purchase> testList;
 
     @Test
     void testFindMaxCategoryMethod() {
