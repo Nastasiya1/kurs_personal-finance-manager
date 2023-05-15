0485b455901455b64278da2c97e9710dd4fef1d7
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2023-05-15 18:57:26 +0300

    Calendar changed to LocalDate; 'categories.tsv' file reading optimized

--- src/main/java/ru/netology/Purchase.java
+++ src/main/java/ru/netology/Purchase.java
@@ -1,13 +1,8 @@
 package ru.netology;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Purchase implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -15,7 +10,6 @@ public class Purchase implements Serializable {
     private String date;
     private int sum;
     private String category;
-    private Calendar calendar;
     private int year;
     private int month;
     private int day;
@@ -35,7 +29,7 @@ public class Purchase implements Serializable {
     }
 
     public String getCategory() {
-        return this.category = addTypeOfCategory(this);
+        return category;
     }
 
     public int getYear() {
@@ -50,27 +44,16 @@ public class Purchase implements Serializable {
         return day;
     }
 
-    private String addTypeOfCategory(Purchase purchase) {
-        try (Scanner scanner = new Scanner(new File("categories.tsv"))) {
-            while (scanner.hasNextLine()) {
-                String[] parts = scanner.nextLine().split("\t");
-                if (purchase.getTitle().equals(parts[0])) {
-                    return parts[1];
-                }
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл с категориями не найден");
-            e.printStackTrace();
-        }
-        return "другое";
+    public void setCategory(String category) {
+        this.category = category;
     }
-    public Purchase specify() throws ParseException {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new SimpleDateFormat("yyyy.MM.dd").parse(date));
-        this.calendar = cal;
-        this.year = calendar.get(Calendar.YEAR);
-        this.month = calendar.get(Calendar.MONTH);
-        this.day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    public Purchase specify() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        LocalDate date = LocalDate.parse(this.date, formatter);
+        this.year = date.getYear();
+        this.month = date.getMonthValue();
+        this.day = date.getDayOfMonth();
         return this;
     }
 }
